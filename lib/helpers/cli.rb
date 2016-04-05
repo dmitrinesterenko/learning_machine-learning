590f6f9f02519ab6d1c81b55014c3b604ad4dc4e
Dmitri Nesterenko <dneseterenko@xogrp.com>
2016-04-04 22:38:37 -0400

    Added commmand line interface
    Specs are commented out as either the specs would work or the
    code would run but not both at the same time
    Need a new way of approaching the testing and TDDing of CLI commands

--- lib/helpers/cli.rb
+++ lib/helpers/cli.rb
@@ -1,11 +1,48 @@
+#this could be useful
+#require 'io/console'
+#rows, columns = $stdout.winsize
+#puts "Your screen is #{columns} wide and #{rows} tall"
+
 class Classification::CLI
-  def initialize(input=STDIN, output=STDOUT)
+  def initialize(input=$stdin, output=$stdout)
     @input = input
     @output = output
+    trainer = Classification::Trainer.new DataSets::Names.data
+    puts DataSets::Names.data
+    learner = Learners::Ngram.new 2, trainer.data
+    @classifier = Classification::Classifier.new learner, trainer
+  end
+
+  def start
+    loop do
+      break unless running?
+      name
+      input = read
+      write_score @classifier.classify(input)
+      correct?
+      new_score  = read
+      write_score @classifier.rescore(input,new_score.to_i)
+      write_score @classifier.classify(input)
+    end
+  end
+
+  def running?
+    true
   end
 
   def name
-    @output << "Please provide a name"
-    puts @output.string
+    @output.puts "Please provide a name"
+  end
+
+  def read
+    @input.gets.chomp
+  end
+
+  def write_score(score)
+    @output.puts "I got #{score}"
+  end
+
+  def correct?
+    @output.puts "Am I correct?"
   end
 end
